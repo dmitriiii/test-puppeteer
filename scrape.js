51d883bb6eed56765ee0257b261876d23c7731cb
dmitriiii <dimitriy1988wl@gmail.com>
2021-09-30 23:19:20 +0300

    first commit

---
+++ scrape.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+
+let scrape = async () => {
+    const browser = await puppeteer.launch({
+        // headless: false
+    });
+    const page = await browser.newPage();
+
+    // await page.goto('http://books.toscrape.com/');
+    // await page.click('#default section ol > li:nth-child(2) .product_pod > div.image_container > a > img');
+    await page.goto('https://showroom.hyundai.ru/');
+    await page.waitFor(1000);
+
+    const result = await page.evaluate(() => {
+        let title = document.querySelector('.main .cars-container>div>div>.title').innerText;
+        // let price = document.querySelector('.m5-coupMng .gridd .gridd__wrr:nth-child(2) .gridd__body>p').innerText;
+
+        return {
+            title
+            // price
+        }
+
+    });
+
+    browser.close();
+    return result;
+};
+
+scrape().then((value) => {
+    console.log(value); // Получилось!
+});
